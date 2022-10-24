f36050c7330643b325c7c11509f2566a17b46777
nate-mal <natanaelmalita@gmail.com>
2022-10-24 23:37:54 +0300

    set menu to play only on updates not on initial mount

--- src/components/ui/AnimDrawerIcon.js
+++ src/components/ui/AnimDrawerIcon.js
@@ -2,14 +2,20 @@ import { useRef, useEffect } from "react";
 import Lottie from "lottie-react";
 import animationData from "../../animations/drawerIcon/data";
 const AnimDrawerIcon = (props) => {
+  const isInitialMount = useRef(true);
   const lottieRef = useRef();
   const { open } = props;
 
   useEffect(() => {
-    if (open) {
-      lottieRef.current.playSegments([0, 50], true);
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
     } else {
-      lottieRef.current.playSegments([50, 90], true);
+      //  useEffect code here to be run on update
+      if (open) {
+        lottieRef.current.playSegments([0, 50], true);
+      } else {
+        lottieRef.current.playSegments([50, 90], true);
+      }
     }
   }, [open]);
   return (
