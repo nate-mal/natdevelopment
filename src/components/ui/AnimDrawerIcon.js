c3faf605b8828ca07084700838b9abcb7d0cb073
nate-mal <natanaelmalita@gmail.com>
2022-10-25 19:44:30 +0300

    fix anim drawer refresh bug

--- src/components/ui/AnimDrawerIcon.js
+++ src/components/ui/AnimDrawerIcon.js
@@ -1,19 +1,19 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Lottie from "lottie-react";
 import animationData from "../../animations/drawerIcon/data";
 const AnimDrawerIcon = (props) => {
   const isInitialMount = useRef(true);
   const lottieRef = useRef();
+  const [drawerIsClose, setDrawerIsClose] = useState(true);
   const { open } = props;
 
   useEffect(() => {
-    if (props.mountIsSet) {
-      //  useEffect code here to be run on update
-      if (open) {
-        lottieRef.current.playSegments([0, 50], true);
-      } else {
-        lottieRef.current.playSegments([50, 90], true);
-      }
+    if (open && drawerIsClose) {
+      setDrawerIsClose(false);
+      lottieRef.current.playSegments([0, 50], true);
+    } else if (!drawerIsClose) {
+      setDrawerIsClose(true);
+      lottieRef.current.playSegments([50, 90], true);
     }
   }, [open]);
   return (
